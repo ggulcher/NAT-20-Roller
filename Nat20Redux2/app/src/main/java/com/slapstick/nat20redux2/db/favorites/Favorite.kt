b3c712474a32195614568e95e44c3c79835eecae
ggulcher <ggulcher1@gmail.com>
2021-02-05 16:13:28 -0500

    Further adjustments

--- Nat20Redux2/app/src/main/java/com/slapstick/nat20redux2/db/favorites/Favorite.kt
+++ Nat20Redux2/app/src/main/java/com/slapstick/nat20redux2/db/favorites/Favorite.kt
@@ -1,10 +1,12 @@
 package com.slapstick.nat20redux2.db.favorites
 
 import android.os.Parcelable
+import androidx.annotation.Keep
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import kotlinx.android.parcel.Parcelize
 
+@Keep
 @Parcelize
 @Entity(tableName = "favorites_table")
 data class Favorite(
